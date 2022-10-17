381b92516daf62900ed1d1f0f1d2f55935e5a9dd
Krushna Jena <krushnajena@appman.in>
2022-10-17 23:54:32 +0530

    Add project files.

---
+++ AppmanBackUpPlus/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace AppmanBackUpPlus
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.cb_windowsauth = new System.Windows.Forms.CheckBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_server = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_backup = new System.Windows.Forms.Button();
+            this.txt_backuplocation = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_reset);
+            this.groupBox1.Controls.Add(this.btn_login);
+            this.groupBox1.Controls.Add(this.cb_windowsauth);
+            this.groupBox1.Controls.Add(this.txt_password);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txt_username);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txt_server);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1080, 64);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Login";
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.BackColor = System.Drawing.Color.Black;
+            this.btn_reset.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_reset.Location = new System.Drawing.Point(952, 19);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(116, 36);
+            this.btn_reset.TabIndex = 8;
+            this.btn_reset.Text = "Reset";
+            this.btn_reset.UseVisualStyleBackColor = false;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
+            // 
+            // btn_login
+            // 
+            this.btn_login.BackColor = System.Drawing.Color.DarkBlue;
+            this.btn_login.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_login.Location = new System.Drawing.Point(794, 19);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(158, 36);
+            this.btn_login.TabIndex = 7;
+            this.btn_login.Text = "Log In and Get Databases";
+            this.btn_login.UseVisualStyleBackColor = false;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // cb_windowsauth
+            // 
+            this.cb_windowsauth.AutoSize = true;
+            this.cb_windowsauth.Location = new System.Drawing.Point(643, 27);
+            this.cb_windowsauth.Name = "cb_windowsauth";
+            this.cb_windowsauth.Size = new System.Drawing.Size(145, 19);
+            this.cb_windowsauth.TabIndex = 6;
+            this.cb_windowsauth.Text = "Windows Authintation";
+            this.cb_windowsauth.UseVisualStyleBackColor = true;
+            this.cb_windowsauth.CheckedChanged += new System.EventHandler(this.cb_windowsauth_CheckedChanged);
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(519, 25);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(118, 23);
+            this.txt_password.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(456, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // txt_username
+            // 
+            this.txt_username.Location = new System.Drawing.Point(316, 25);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(134, 23);
+            this.txt_username.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(245, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "User Name";
+            // 
+            // txt_server
+            // 
+            this.txt_server.Location = new System.Drawing.Point(67, 25);
+            this.txt_server.Name = "txt_server";
+            this.txt_server.Size = new System.Drawing.Size(172, 23);
+            this.txt_server.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_backup);
+            this.groupBox2.Controls.Add(this.txt_backuplocation);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox2.Location = new System.Drawing.Point(0, 506);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1080, 61);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "BackUp";
+            // 
+            // btn_backup
+            // 
+            this.btn_backup.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btn_backup.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_backup.Location = new System.Drawing.Point(817, 17);
+            this.btn_backup.Name = "btn_backup";
+            this.btn_backup.Size = new System.Drawing.Size(251, 36);
+            this.btn_backup.TabIndex = 8;
+            this.btn_backup.Text = "Back Up";
+            this.btn_backup.UseVisualStyleBackColor = false;
+            this.btn_backup.Click += new System.EventHandler(this.btn_backup_Click);
+            // 
+            // txt_backuplocation
+            // 
+            this.txt_backuplocation.Location = new System.Drawing.Point(117, 25);
+            this.txt_backuplocation.Name = "txt_backuplocation";
+            this.txt_backuplocation.Size = new System.Drawing.Size(694, 23);
+            this.txt_backuplocation.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Back Up Loaction";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dataGridView1);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(0, 64);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(1080, 442);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Databases";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.HotTrack;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.Size = new System.Drawing.Size(1074, 420);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1080, 567);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Appman Backup Plus";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.CheckBox cb_windowsauth;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_server;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_backup;
+        private System.Windows.Forms.TextBox txt_backuplocation;
+        private System.Windows.Forms.Button btn_reset;
+    }
+}
---
+++ AppmanBackUpPlus/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Common;
+using System.IO;
+
+namespace AppmanBackUpPlus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            groupBox2.Visible = false;
+            dataGridView1.Visible = false;
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txt_server.Text != "")
+                {
+                    if (cb_windowsauth.Checked == true)
+                    {
+                        string connectionString;
+                        if (cb_windowsauth.Checked == true)
+                        {
+                            connectionString = "Data Source=" + txt_server.Text + ";Trusted_Connection=true";
+                        }
+                        else
+                        {
+                            connectionString = "Data Source=" + txt_server.Text + "; user id=" + txt_username.Text + ";password=" + txt_password.Text;
+                        }
+                        using (SqlConnection con = new SqlConnection(connectionString))
+                        {
+                            con.Open();
+                            using (SqlCommand cmd = new SqlCommand("SELECT name from sys.databases where name not in('tempdb', 'master','model','msdb')", con))
+                            {
+
+                                using (SqlDataReader dr = cmd.ExecuteReader())
+                                {
+                                    DataTable dtCustomers = new DataTable();
+                                    dtCustomers.Load(dr);
+                                    dataGridView1.DataSource = dtCustomers;
+                                    groupBox2.Visible = true;
+                                    txt_server.Enabled = false;
+                                    txt_username.Enabled = false;
+                                    txt_password.Enabled = false;
+                                    cb_windowsauth.Enabled = false;
+                                    dataGridView1.Visible = true;
+
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if(txt_username.Text !="" && txt_password.Text != "")
+                        {
+                            string connectionString;
+                            if (cb_windowsauth.Checked == true)
+                            {
+                                connectionString = "Data Source=" + txt_server.Text + ";Trusted_Connection=true";
+                            }
+                            else
+                            {
+                                connectionString = "Data Source=" + txt_server.Text + "; user id=" + txt_username.Text + ";password=" + txt_password.Text;
+                            }
+                            using (SqlConnection con = new SqlConnection(connectionString))
+                            {
+                                con.Open();
+                                using (SqlCommand cmd = new SqlCommand("SELECT name from sys.databases where name not in('tempdb', 'master','model','msdb')", con))
+                                {
+
+                                    using (SqlDataReader dr = cmd.ExecuteReader())
+                                    {
+                                        DataTable dtCustomers = new DataTable();
+                                        dtCustomers.Load(dr);
+                                        dataGridView1.DataSource = dtCustomers;
+                                        groupBox2.Visible = true;
+                                        txt_server.Enabled = false;
+                                        txt_username.Enabled = false;
+                                        txt_password.Enabled = false;
+                                        cb_windowsauth.Enabled = false;
+                                        dataGridView1.Visible = true;
+
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Please Enter User Name & Password.");
+                        }
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Please Enter Server.");
+                }
+                
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            
+        }
+
+        private void cb_windowsauth_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cb_windowsauth.Checked)
+            {
+                txt_username.Text = "";
+                txt_password.Text = "";
+                txt_username.Enabled = false;
+                txt_password.Enabled = false;
+            }
+            else
+            {
+                txt_username.Enabled = true;
+                txt_password.Enabled = true;
+            }
+        }
+
+        private void btn_backup_Click(object sender, EventArgs e)
+        {
+            
+
+            try
+            {
+                if (txt_backuplocation.Text != "")
+                {
+                    string destinationPath = txt_backuplocation.Text;
+
+                    if (!Directory.Exists(destinationPath))
+                    {
+                        Directory.CreateDirectory(destinationPath);
+                    }
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+                        string databaseName = row.Cells[0].Value.ToString();//dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].FormattedValue.ToString();    
+
+                        //Define a Backup object variable.    
+                        Backup sqlBackup = new Backup();
+
+                        ////Specify the type of backup, the description, the name, and the database to be backed up.    
+                        sqlBackup.Action = BackupActionType.Database;
+                        sqlBackup.BackupSetDescription = "BackUp of:" + databaseName + "on" + DateTime.Now.ToShortDateString();
+                        sqlBackup.BackupSetName = "FullBackUp";
+                        sqlBackup.Database = databaseName;
+
+                        ////Declare a BackupDeviceItem    
+                        //string destinationPath = txt_backuplocation.Text + @"\" + System.DateTime.Now;
+                        string backupfileName = databaseName + "_" + System.DateTime.Now.ToShortDateString() + ".bak";
+                        BackupDeviceItem deviceItem = new BackupDeviceItem(destinationPath + "\\" + backupfileName, DeviceType.File);
+                        ////Define Server connection    
+
+                        //ServerConnection connection = new ServerConnection(frm.serverName, frm.userName, frm.password);    
+                        ServerConnection connection;
+                        if (cb_windowsauth.Checked)
+                        {
+                            connection = new ServerConnection(txt_server.Text);
+                        }
+                        else
+                        {
+                            connection = new ServerConnection(txt_server.Text, txt_username.Text, txt_password.Text);
+
+                        }
+                        ////To Avoid TimeOut Exception    
+                        Server sqlServer = new Server(connection);
+                        sqlServer.ConnectionContext.StatementTimeout = 60 * 60;
+                        Database db = sqlServer.Databases[databaseName];
+
+                        sqlBackup.Initialize = true;
+                        sqlBackup.Checksum = true;
+                        sqlBackup.ContinueAfterError = true;
+
+                        ////Add the device to the Backup object.    
+                        sqlBackup.Devices.Add(deviceItem);
+                        ////Set the Incremental property to False to specify that this is a full database backup.    
+                        sqlBackup.Incremental = false;
+
+                        sqlBackup.ExpirationDate = DateTime.Now.AddDays(3);
+                        ////Specify that the log must be truncated after the backup is complete.    
+                        sqlBackup.LogTruncation = BackupTruncateLogType.Truncate;
+
+                        sqlBackup.FormatMedia = false;
+                        ////Run SqlBackup to perform the full database backup on the instance of SQL Server.    
+                        sqlBackup.SqlBackup(sqlServer);
+                        ////Remove the backup device from the Backup object.    
+                        sqlBackup.Devices.Remove(deviceItem);
+                        // toolStripStatusLabel1.Text = "Successful backup is created!";
+
+
+                    }
+                    MessageBox.Show("All User Database BackedUp Successfully");
+
+                }
+                else
+                {
+                    MessageBox.Show("Please Enter BackUp Location.");
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            txt_server.Enabled = true;
+            txt_username.Enabled = true;
+            txt_password.Enabled = true;
+            cb_windowsauth.Enabled = true;
+            groupBox2.Visible = false;
+            dataGridView1.Visible = false;
+        }
+    }
+}
